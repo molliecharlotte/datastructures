12c3a4e02a37180d6e6ecf3e3d028e5d08b53bb8
Mol <molliecharlottesuss@gmail.com>
2017-01-26 13:07:30 -0500

    Update lab1.java

--- lab1.java
+++ lab1.java
@@ -11,7 +11,58 @@ public class Rectangle()
     
  }
  
- public void Rectangle (int length, int width)
+ public void Rectangle (int l, int w)
+ {
+   length = l;
+   width = w;
+ }
+
+public int getLength()
+{
+  return length;
+}
+
+public int getWidth()
+{
+  return width;
+}
+
+public void setLength(int m)
+{
+  length = m;
+}
+
+public void setWidth(int n)
+{
+ width = n;
+}
+
+public int getArea()
+{
+  int area = length*width;
+  
+  for(i=0, i<10, i++)
+  {
+    System.out.println("The area of the rectangle is" + area);
+  }
+  
+  return area;
+}
+
+public boolean isSquare()
+{
+  if(length==width)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+public class IOTestOut()
+
     
     
     
