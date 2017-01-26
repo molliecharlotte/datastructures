6f83ff30f2b659f952c993b67666abfec1a7c8fc
Mol <molliecharlottesuss@gmail.com>
2017-01-26 12:58:39 -0500

    Update and rename .gitignore to lab1.java

---
+++ lab1.java
@@ -0,0 +1,17 @@
+public class Rectangle()
+{
+  private int length
+  private int width
+  
+  public static Rectangle()
+    {
+      length = 1;
+      width = 1;
+    }
+    
+ }
+ 
+ public void Rectangle (int length, int width)
+    
+    
+    
